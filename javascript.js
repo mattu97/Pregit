85a62d71c891e102e6af52117c9f7749a7ec5bdf
mattu97 <matiasignaciomellado@hotmail.com>
2016-10-02 21:13:55 -0300

    probando

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function cargo(id)
+{
+	if(id==1)
+	{
+		document.getElementById(cargo).innerHTML ="Caja";
+	}
+	if(id==2)
+	{
+		document.getElementById(cargo).innerHTML ="Inventario";
+	}
+	if(id==3)
+	{
+		document.getElementById(cargo).innerHTML ="Proveedor";
+	}
+}
