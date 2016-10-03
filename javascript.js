144e00b7f68b961962393458385abeb2172430cf
mattu97 <matiasignaciomellado@hotmail.com>
2016-10-02 21:23:15 -0300

    ultimo

--- javascript.js
+++ javascript.js
@@ -2,14 +2,14 @@ function cargo(id)
 {
 	if(id==1)
 	{
-		document.getElementById(cargo).innerHTML ="Caja";
+		document.getElementById("cargo").innerHTML ="Caja";
 	}
 	if(id==2)
 	{
-		document.getElementById(cargo).innerHTML ="Inventario";
+		document.getElementById("cargo").innerHTML ="Inventario";
 	}
 	if(id==3)
 	{
-		document.getElementById(cargo).innerHTML ="Proveedor";
+		document.getElementById("cargo").innerHTML ="Proveedor";
 	}
 }
